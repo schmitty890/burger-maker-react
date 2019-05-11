e7550166683f9591d80ffcade3b87ba8bef263d8
jschmitt <schmitty890@gmail.com>
2019-05-11 17:43:34 -0400

    calculating ingredient sum dynamically based on props passed to Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
   // transform the object passed by props into an array of ingredients with 
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
           return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
             return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
           })
-        });
+        })
+        .reduce((arr, el) => {
+          // console.log('arr: ', arr);
+          // console.log('el: ', el);
+          return arr.concat(el);
+        }, []); // now transformedIngredients is a flattened array of our jsx elements
 
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>add some ingredients</p>
+  }
   console.log(transformedIngredients);
   return(
     <div className={classes.Burger}>

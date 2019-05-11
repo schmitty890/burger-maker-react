ac17c7ff010f4027e278d8593f76a9fce35c5e52
jschmitt <schmitty890@gmail.com>
2019-05-11 16:48:11 -0400

    hardcoded burger rendering

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,13 @@
 // BurgerBuilder made in containers folder as we plan on managing state for the burger we will build
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>build controls</div>
+        <Burger />
       </Aux>
     );
   }

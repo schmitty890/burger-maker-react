f41f2db71db3b960018a64fae138c9667ce30451
jschmitt <schmitty890@gmail.com>
2019-05-11 22:21:55 -0400

    showing and hiding order now modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,7 +28,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false
   }
 
   updatePurchaseState(ingredients) {
@@ -79,6 +80,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  }
+
   render() {
     const disableInfo = {
       ...this.state.ingredients
@@ -91,7 +96,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -100,7 +105,8 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler} 
           disable={disableInfo}
           price={this.state.totalPrice}
-          purchaseable={this.state.purchaseable} />
+          purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler} />
       </Aux>
     );
   }

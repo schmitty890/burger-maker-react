b2b8015beec2bb0919c84a9f56b33b9aae7b4d15
jschmitt <schmitty890@gmail.com>
2019-05-12 13:50:56 -0400

    adding a sidedrawer toggle component to animate the sidedrawer flyout on mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,17 +6,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   }
 
+  sideDrawerToggleHandler = () => {
+    // clean way of setting the state when it depends on the old state
+    this.setState((prevState) => {
+      return this.setState({ showSideDrawer: !prevState.showSideDrawer });
+    });
+  }
+
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer 
           open={this.state.showSideDrawer} 
           closed={this.sideDrawerClosedHandler} />
